e17dd3fe9724a0f4db02216836fde898f15bcc26
Evan Zhao <hzhao5@uw.edu>
2024-07-09 11:39:35 -0700

    Initial project commit

---
+++ PerfectPitchTraining/PerfectPitchTraining/PerfectPitchTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectPitchTrainingApp.swift
+//  PerfectPitchTraining
+//
+//  Created by Evan Zhao on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PerfectPitchTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
