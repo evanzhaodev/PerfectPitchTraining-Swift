6b4283312dd33a0d3691773d8da4eb1d204bae43
Evan Zhao <hzhao5@uw.edu>
2024-07-09 19:00:40 -0700

    Add random note generator

--- PerfectPitchTraining/PerfectPitchTraining/ContentView.swift
+++ PerfectPitchTraining/PerfectPitchTraining/ContentView.swift
@@ -6,16 +6,61 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var debugLog: String = ""
+    @State private var audioPlayer: AVAudioPlayer?
+    private var pitches: [String] {
+        var pitchBuilder = [String]()
+        for i in 1...7 {
+            pitchBuilder.append("\(i)-a")
+            pitchBuilder.append("\(i)-as")
+            pitchBuilder.append("\(i)-b")
+            pitchBuilder.append("\(i)-c")
+            pitchBuilder.append("\(i)-cs")
+            pitchBuilder.append("\(i)-d")
+            pitchBuilder.append("\(i)-ds")
+            pitchBuilder.append("\(i)-e")
+            pitchBuilder.append("\(i)-f")
+            pitchBuilder.append("\(i)-fs")
+            pitchBuilder.append("\(i)-g")
+            pitchBuilder.append("\(i)-gs")
         }
-        .padding()
+        return pitchBuilder
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    Button("Generate a new pitch") { playSound() }
+                }
+                
+                Section {
+                    Text("Log: \(debugLog)")
+                }
+            }
+            .navigationTitle("Perfect Pitch Training")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    func playSound() {
+        guard let pitchFileName = pitches.randomElement(),
+              let url = Bundle.main.url(forResource: pitchFileName, withExtension: "wav") else {
+            print("File not found")
+            debugLog = "File not found"
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound \(error.localizedDescription)")
+            debugLog = "Error playing sound \(error.localizedDescription)"
+        }
+        debugLog = "Successful"
     }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PerfectPitchTraining-Swift
 This is a project I will use to practice my SwiftUI skills
+
+Audio files credit: https://www.youtube.com/watch?v=TLKTJRyqlD0
